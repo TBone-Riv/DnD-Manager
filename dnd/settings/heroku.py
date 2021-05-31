6d979ea06274349dd5ad98c53e108a4f3327c670
Thibaut Rivolet <trivolet@gmail.com>
2021-05-31 07:51:05 +0200

    static erro

--- dnd/settings/heroku.py
+++ dnd/settings/heroku.py
@@ -138,9 +138,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 AUTH_USER_MODEL = 'profile.CustomUser'
 
