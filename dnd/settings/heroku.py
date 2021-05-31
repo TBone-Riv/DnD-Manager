7b1e5cb25cd3cdc9729ca327c3f1af554cc3b7b0
Thibaut Rivolet <trivolet@gmail.com>
2021-05-31 07:04:33 +0200

    heroku setup

---
+++ dnd/settings/heroku.py
@@ -0,0 +1,14 @@
+import os
+
+ALLOWED_HOSTS = ['https://p13-dnd-manager.herokuapp.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DATABASE_URL'],
+        'PORT': '5432',
+    }
+}
